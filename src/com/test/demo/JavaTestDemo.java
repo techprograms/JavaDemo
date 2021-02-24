39984f42de20a818620adb3546d41b58eabbe3d9
technoprograms <tech.gpr@gmail.com>
2021-02-24 12:52:24 +0530

    descriptive comment for code change

---
+++ src/com/test/demo/JavaTestDemo.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+public class JavaTestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello welcome in Java World!!");
+	}
+
+}
